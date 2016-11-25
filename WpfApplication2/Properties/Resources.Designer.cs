3d80f05f34009196337aa784e2505c5efe039c7a
RoanWC <Roanwc@gmail.com>
2016-11-25 17:48:55 -0500

    barrier

--- WpfApplication2/Properties/Resources.Designer.cs
+++ WpfApplication2/Properties/Resources.Designer.cs
@@ -123,9 +123,9 @@ namespace WpfApplication2.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap ship {
+        internal static System.Drawing.Bitmap starfield {
             get {
-                object obj = ResourceManager.GetObject("ship", resourceCulture);
+                object obj = ResourceManager.GetObject("starfield", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
